e1f708c39884b47228a09b54f93700055ada843d
TahelZecharia <93916792+TahelZecharia@users.noreply.github.com>
2021-11-29 22:45:05 +0200

    comments

--- my_mat.c
+++ my_mat.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
-// int Mat[10][10];
 
+//The function captures the values of the matrix from the user
+//and stores in each cell of the matrix that it receives the
+//shortest path that exists between i and j
 int dinamicProgramMatrics(int MATRIX[10][10]){
     
     int k, i, j;
     
+    //Initialization of the matrix by the values entered by the user.
     for (i = 0 ; i < 10 ; i++){
         for (j = 0 ; j < 10 ; j++){
             scanf("%d", &MATRIX[i][j]);
         }
     }
         
-    
+    //Dynamic design using the Floyd – Warshall Algorithm.
     for (k = 0 ; k < 10 ; k++){
         for (i = 0 ; i < 10 ; i++){
             for (j = 0 ; j < 10 ; j++){
-                if ( (MATRIX[i][k] != 0) && (MATRIX[k][j] != 0) ) {
-                    // printf ("first: %d", MATRIX[i][k]);
-                    // printf ("second: %d", MATRIX[k][j]);
+                //making sure none of them are worth infinity.
+                if ( (MATRIX[i][k] != 0) && (MATRIX[k][j] != 0) ) 
+                {
                     if (MATRIX[i][j] == 0 && (i != j)) 
                     {
                         MATRIX[i][j] = (MATRIX[i][k] + MATRIX[k][j]);
@@ -32,49 +35,24 @@ int dinamicProgramMatrics(int MATRIX[10][10]){
         }
         
     }
-    return 1;
+    return 0;
 }
 
+//The program captures the values i and j from the user and prints
+//"True" if there is a path from i to j. Otherwise, print "False".
 int thereIsRoute(int MATRIX[10][10], int i, int j){
     if (MATRIX[i][j] == 0) printf("False\n");
     else printf("True\n");
-    return 1;
+    return 0;
 }
 
+//The program receives from the user the values i and j and prints the
+//shortest route from i to j, if there is no route the program will print -1.
 int bestRoute(int MATRIX[10][10], int i, int j){
     if (MATRIX[i][j] == 0) printf("%d\n", -1);
     else printf("%d\n", MATRIX[i][j]);
-    return 1;
+    return 0;
 }
 
-// int main(){
-//     int i,j;
-//     char letter = 'E';
-//     while (letter != 'D')
-//     {
-//         // scan the input char
-//         scanf("%c", &letter);
-
-//         // 1) if the char is A:
-//         if (letter == 'A')
-//         {
-//             dinamicProgramMatrics(Mat);
-//         }
-//         // 2) if the char is B:
-//         else if (letter == 'B')
-//         {
-//             scanf("%d", &i);
-//             scanf("%d", &j);
-//             thereIsRoute(Mat, i, j);
-//         }
-//         // 3) if the char is C:
-//         else if (letter == 'C')
-//         {
-//             scanf("%d", &i);
-//             scanf("%d", &j);
-//             bestRoute(Mat, i, j);
-//         }   
-        
-//     }
     
 // }
