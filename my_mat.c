434577cd052fe86ea55eed908b74f07b07cbd82b
TahelZecharia <93916792+TahelZecharia@users.noreply.github.com>
2021-11-29 22:22:18 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -16,10 +16,12 @@ int dinamicProgramMatrics(int MATRIX[10][10]){
         for (i = 0 ; i < 10 ; i++){
             for (j = 0 ; j < 10 ; j++){
                 if ( (MATRIX[i][k] != 0) && (MATRIX[k][j] != 0) ) {
-                    // if (MATRIX[i][j] == 0) 
-                    // {
-                    //     MATRIX[i][j] = (MATRIX[i][k] + MATRIX[k][j]);
-                    // }
+                    // printf ("first: %d", MATRIX[i][k]);
+                    // printf ("second: %d", MATRIX[k][j]);
+                    if (MATRIX[i][j] == 0 && (i != j)) 
+                    {
+                        MATRIX[i][j] = (MATRIX[i][k] + MATRIX[k][j]);
+                    }
                     if (MATRIX[i][j] > ( MATRIX[i][k] + MATRIX[k][j]) )
                     {
                          MATRIX[i][j] = MATRIX[i][k] + MATRIX[k][j];
